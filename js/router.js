73187020ffb98b4ec7b60c8367c5e47b40d67546
Will McNeilly <wmcneilly@rumblelabs.com>
2013-06-04 08:45:35 +0100

    Add a pomodoros.new route

--- js/router.js
+++ js/router.js
@@ -1,5 +1,6 @@
 App.Router.map(function() {
   this.resource('pomodoros', { path: "pomodoros" }, function(){
+  	this.route('new');
   	this.resource('pomodoro', { path: '/:pomodoro_id' });
   });
   this.resource('settings', { path: "settings" });
--- js/routes/index-route.js
+++ js/routes/index-route.js
@@ -1,5 +1,5 @@
 App.IndexRoute = Ember.Route.extend({
 	redirect: function () {
-    this.transitionTo('pomodoro');
+    this.transitionTo('pomodoros');
   }
 });
