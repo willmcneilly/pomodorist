fc646b35fae41af15acb451afe7889b894b746fd
Will McNeilly <wmcneilly@rumblelabs.com>
2013-06-05 07:59:48 +0100

    When pomodoro is created transition to that particular model view

--- js/controllers/pomodoros-new-controller.js
+++ js/controllers/pomodoros-new-controller.js
@@ -1,6 +1,6 @@
 App.PomodorosNewController = Ember.ObjectController.extend({
   createPomodoro: function() {
   	this.get('model.transaction').commit();
-  	this.get("target").transitionTo('pomodoros');
+  	this.transitionToRoute('pomodoros.pomodoro', this.get('model'));
   },
 });
